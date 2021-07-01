7c41ab61f8145e7e300e6b01992859049ad44715
Ilja Patrushev <slowtnr@gmail.com>
2021-07-01 14:48:45 +0300

    Initial Commit

---
+++ TutorialMegaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TutorialMegaApp
+//
+//  Created by Ilja Patrushev on 1.7.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
